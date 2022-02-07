a4b9788d1d1be78877ffec41a2c91a3e19007c25
vdumbrava <vladislav.dumbrava@gmail.com>
2022-02-07 13:05:57 +0200

    fix build

--- projects/ng2-forms/src/public-api.ts
+++ projects/ng2-forms/src/public-api.ts
@@ -7,3 +7,6 @@ export * from './lib/error-token';
 export * from './lib/mark-nested-as-touched';
 export * from './lib/mark-nested-as-untouched';
 export * from './lib/ng-forms.module';
+export * from './lib/equal.directive';
+export * from './lib/error-message.pipe';
+export * from './lib/error.pipe';
